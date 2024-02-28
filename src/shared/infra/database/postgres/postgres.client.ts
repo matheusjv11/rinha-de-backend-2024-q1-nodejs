e4a3d863712d7bfe8e3ed6fc298856f83b067720
Matheus Almeida <matheusjv14@gmail.com>
2024-02-28 08:28:55 -0300

    Updates on docker config

--- src/shared/infra/database/postgres/postgres.client.ts
+++ src/shared/infra/database/postgres/postgres.client.ts
@@ -1,16 +1,12 @@
 import { Pool, PoolClient } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgres://admin:123@localhost:5432/rinha",
+  connectionString: "postgres://admin:123@db:5432/rinha",
 });
 
 let client: PoolClient | null = null;
 
 export const connect = async () => {
-  if (client) {
-    return client;
-  }
-
   client = await pool.connect();
 
   return client;
